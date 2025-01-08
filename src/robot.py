6a3834604ef89e676db1d8567e4d8e33fbd943f5
Kenneth E. Bellock <ken@bellock.net>
2025-01-08 08:09:59 -0500

    Including dashboard for running the robot

--- src/robot.py
+++ src/robot.py
@@ -1,13 +1,32 @@
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor
-from pybricks.parameters import Direction, Port, Stop
+from pybricks.parameters import Direction, Port
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait, multitask, run_task, StopWatch
-from config import *
+from pybricks.tools import wait, multitask, run_task
+from config import (
+    LEFT_MOTOR_PORT,
+    RIGHT_MOTOR_PORT,
+    BACK_MOTOR_PORT,
+    FRONT_MOTOR_PORT,
+    LEFT_COLOR_SENSOR_PORT,
+    RIGHT_COLOR_SENSOR_PORT,
+    WHEEL_DIAMETER,
+    AXLE_TRACK,
+    STRAIGHT_SPEED,
+    STRAIGHT_ACCELERATION,
+    TURN_RATE,
+    TURN_ACCELERATION)
 
 
-def diff_angles(self, a, b):
+def diff_angles(a: int, b: int):
     return ((b - a) + 180) % 360 - 180
+
+
+def dict2json(dictionary: dict):
+    return '{' + ', '.join([
+        '"' + key + '": ' + ['', '"'][int(isinstance(value, str))] + str(
+            value) + ['', '"'][int(isinstance(
+                value, str))] for key, value in dictionary.items()]) + '}'
 
 
 class Robot():
@@ -29,7 +48,7 @@ class Robot():
         # Initialize Color Sensors
         self._color_sensors = {
             'left': ColorSensor(getattr(Port, LEFT_COLOR_SENSOR_PORT)),
-            'right': ColorSensor(getattr(Port, LEFT_COLOR_SENSOR_PORT))}
+            'right': ColorSensor(getattr(Port, RIGHT_COLOR_SENSOR_PORT))}
 
         # Initialize the drive base
         self._drive_base = DriveBase(
@@ -41,20 +60,46 @@ class Robot():
             STRAIGHT_ACCELERATION,
             TURN_RATE,
             TURN_ACCELERATION)
+        self._drive_base.use_gyro(True)
+        self.trigger: bool = True
         self.queue = []
+
+    async def mainandlog(self):
+        await multitask(
+            self.main(),
+            self.logit())
 
     async def main(self):
         for callable, args, kwargs in self.queue:
             await callable(*args, **kwargs)
+        self.trigger = False
+
+    async def logit(self):
+        while self.trigger:
+            print(dict2json({
+                # 'Left Color Sensor': self._color_sensors[
+                #     'left'].reflection(),
+                # 'Right Color Sensor': self._color_sensors[
+                #     'right'].reflection(),
+                'Right Motor Angle': self._motors['right'].angle(),
+                # 'Right Motor Speed': self._motors['right'].speed(),
+                'Left Motor Angle': self._motors['left'].angle(),
+                # 'Left Motor Speed': self._motors['left'].speed(),
+                'Front Motor Angle': self._motors['front'].angle(),
+                # 'Front Motor Speed': self._motors['front'].speed(),
+                'Back Motor Angle': self._motors['back'].angle(),
+                # 'Back Motor Speed': self._motors['back'].speed()}))
+                }))
+            await wait(500)
 
     def run(self):
-        run_task(self.main())
+        run_task(self.mainandlog())
 
     def reset_heading(self):
         self._hub.imu.reset_heading()
 
     def wait(self, time):
-        wait(time)
+        self.queue.append((wait, (time,), {}))
 
     def heading(self):
         return self._hub.imu.heading()
@@ -66,7 +111,9 @@ class Robot():
         self.queue.append((self._drive_base.turn, (angle,), {}))
 
     def back_lift(self, angle, speed=120):
-        self.queue.append((self._motors['back'].run_angle, (speed, angle,), {}))
+        self.queue.append(
+            (self._motors['back'].run_angle, (speed, angle,), {}))
 
     def front_lift(self, angle, speed=120):
-        self.queue.append((self._motors['front'].run_angle, (speed, angle,), {}))
+        self.queue.append(
+            (self._motors['front'].run_angle, (speed, angle,), {}))
