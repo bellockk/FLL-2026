e0a1b3ed495bfa77b14f59e2a2b39cdad3447ad2
Riley Bellock <bellockriley@mail.com>
2025-01-16 18:34:22 -0500

    Capturing progress

--- src/run.py
+++ src/run.py
@@ -2,13 +2,14 @@ from robot import Robot
 
 robot = Robot()
 
+
 # Drive to Sea Creature and Catch the creature
 robot.drive(140)
 robot.turn(-50)
 robot.drive(350)
 robot.drive(-440)
 
-# Remove the sea creature while the robot is back in the start area
+#  Krill and Coral
 robot.drive(200)
 robot.turn(50)
 robot.fork_lift(120, 2000)
@@ -16,66 +17,48 @@ robot.plow_lift(350, 2000)
 robot.fork_lift(-120, 2000)
 robot.drive(400)
 robot.drive(-300)
-robot.turn(-135)
+
+# Yellow Boat
+robot.turn(-137)
 robot.drive(120)
 robot.fork_lift(350, 2000)
 robot.drive(-80)
 robot.fork_lift(-100)
 robot.turn(40)
 robot.fork_lift(-90, 2000)
-robot.turn(35)
-robot.drive(350)
-
-#robot.turn(60)
-#robot.drive(400)
-#robot.drive(-200)
-#robot.turn(55)
-#robot.drive(360)
-
-# WHO TOOK THE COOKIE FROM THE COOKIE JAR? CONNOR TOOK THE COOKIE FROM THE
-# COOKIE JAR!
-
-"""
-robot.fork_lift(350, 2000)
-robot.drive(-180)
-robot.fork_lift(-80)
-robot.turn(90)
-
-#collection 2
-robot.back_lift(120,2000)
-robot.front_lift(350,2000)
-robot.back_lift(-150,2000)
-robot.drive(570)
-robot.turn(60)
-robot.drive(60)
+robot.turn(115)
+robot.drive(315)
+robot.turn(-100)
+robot.drive(90)
+robot.fork_lift(150,2000)
+#robot.turn(-10)
+#robot.drive(550)
+#robot.turn(20)
+#robot.drive(200)
 
 
+#robot.turn(-15)
+#robot.drive(100)
+#robot.turn(10)
+#robot.drive(500)
+#robot.turn(15)
+#robot.drive(50)
+#robot.turn(-15)
+#robot.drive(200)
 
-
-#robot.drive(-500)
-
-
-#realignment 2
-robot.turn(-60)
-robot.drive(50)
-robot.drive(-250)
-robot.front_lift(-175,2000)
-robot.turn(50)
-robot.drive(-220)
-robot.turn(-90)
-robot.back_lift(350,2000)
-robot.front_lift(-175,2000)
-
-
-#yellow boat
-robot.turn(-110)
-robot.drive(-80)
-robot.back_lift(-80)
-robot.turn(90)
-
-
-#Realigmment 3 """
-
-
+'''
+robot.drive(200)
+robot.turn(147)
+robot.drive(-185)
+robot.fork_lift(350,2000)
+robot.drive(520)
+robot.fork_lift(-350,2000)
+robot.turn(-30)
+robot.drive(-500)
+robot.turn(-30)
+robot.drive(-200)
+robot.turn(-40)
+robot.fork_lift(350, 500)
+'''
 # !! DO NOT REMOVE THE FOLLOWING LINE !!
 robot.run()
