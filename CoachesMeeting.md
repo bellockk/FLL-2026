c7fca7dba38ecbfc8ea5a7d5d951162a50d7f17c
Kenneth E. Bellock <ken@bellock.net>
2025-02-20 17:04:38 -0500

    Capturing progress

---
+++ CoachesMeeting.md
@@ -0,0 +1,23 @@
+# Coaches Meeting FLL MD State Tournament Coaches Meeting (2/19/2025)
+
+## Notes
+
+* A printed roster is required for check-in
+* Parking is free
+* Go to recreactional facility
+* Check website for the state event
+* On Check-in we will receive tickets for the practice round
+* Wrist bands will have the team number, which will be required to get in the pit
+* One coach in the judges meeting
+* Parents cannot be in the pits (fire safety rule, they will be checking)
+* Chick-fil-a a short walk away from 11am - 5pm count on lines as multiple events will be hapening
+* 6 Chairs, cannot bring our own
+* Eating/drinking allowed in the pit area, but not the arena
+* Tickets are used to reserve time on the practice tables
+* Judges are walking the floor, and they will report anything they see for team scoring
+* Queue up 15 minutes before your time
+* Watch noise levels!  Judges are watching, especially when walking by juding/competing areas
+* All teams and spectators should be in the stands for the opening ceremony
+* Judges keep a close eye on the practice tables in the pits to see how teams treat the previous and incoming teams
+* Listen to judges!  If judges tell you where you can stand and you do not listen, they will dock all "gracious professionalism" points
+* 14 teams will proceed to world championships  9 teams will advance, 5 teams will get an invitational
--- src/run.py
+++ src/run.py
@@ -5,6 +5,7 @@ robot = Robot()
 def run1():
     # Pre-Run Initialization/Calibration
     robot.initialize()
+    robot.raise_all()
 
     ## 0s 89pts  0deg is West
 
@@ -61,12 +62,12 @@ def run2():
     # s 164pts
 
     # Reveal East Whale (s 20pts)
-    robot.turn(-10)      # -150
+    robot.turn(-15)      # -155
     robot.drive(170)
-    robot.plow(20, 300)
-    robot.curve(-300, 30) #  -180
+    robot.plow(20, 500)
+    robot.curve(-400, 25) #  -180
     robot.drive(-40)
-    robot.drive(30)
+    robot.drive(40)
 
     # s 184pts
 
@@ -75,45 +76,59 @@ def run2():
 
     # s 194pts
 
-    #  Angler Fish (s 30pts)
+    # Send over the Submersible (s 30pts)
     robot.plow(40, 300)
     robot.curve(100, -45)  #  -150
     robot.curve(200, 45)  #  -180
     robot.plow_lower()
-    robot.drive(200)
-#     robot.curve(850, -20)  #  -155
-#     robot.curve(150, 25)  #  -155
-#     robot.curve(300, 20)  #  -155
-#     robot.curve(300, -25)  #  -155
-#     robot.drive(200)
-#     robot.drive(-500)
-#     robot.plow(30)
-#     robot.curve(200, 52)  #  -155
-#     robot.curve(150, -52)  #  -155
-#     robot.drive(200)
-#     robot.plow_lower()
-#     robot.plow(30)
+    robot.drive(125)
+    robot.curve(100, 40)  #  -180
 
-    # s 234pts
+    # s 224pts
 
-    # Send over the Submersible (s 30pts)
+    #  Angler Fish (s 30pts)
+    robot.curve(200, -85)  #  -180
+    robot.curve(100, 70)  #  -180
+    robot.curve(100, -25)  #  -180
 
-    # s 264pts
-
-    # Collect Seabed Sample (s 10pts)
-
-    # s 274pts
-
-    # Flip up Coral Reef (s 20pts)
-
-    # s 294pts
+    # s 254pts
 
     # Drive Off Board with Krill, samples, and Coral Tree (s -10pts)
+    robot.drive(100)
+    robot.curve(400, -70)
+    #robot.drive(300)
+    # robot.curve(800, 70)
 
-    # s 284pts
+
+    # s 244pts
+
+
 
 def run3():
-    pass
+    robot.fork_lift(60)
+    robot.turn(-97)
+    robot.drive(-250)
+    # robot.fork_lift(52)
+    robot.fork_lift(50)
+    robot.drive(150)
+    robot.turn(50)
+    robot.drive(-420)
+    robot.turn(227)
+    robot.plow_lower()
+    robot.drive(260)
+    robot.drive(-200)
+    robot.plow(50)
+    robot.turn(-45)
+    robot.drive(140)
+    robot.turn(45)
+    robot.drive(-120)
+    robot.fork_lift(55)
+    robot.curve(100, 88)
+    robot.fork_lift(53)
+    robot.drive(-100)
+    robot.fork_lift(48)
+    #robot.turn(97)
+    #robot.curve(-330, 140)
 
     # Hang Coral Tree (s 20pts)
 
@@ -143,6 +158,15 @@ def run3():
 
     # s 454pts
 
+    # Collect Seabed Sample (s 10pts)
+
+    # s 274pts
+
+    # Flip up Coral Reef (s 20pts)
+
+    # s 294pts
+
+
 def run4():
     pass
 
@@ -153,29 +177,43 @@ def run4():
     # Get Trident Piece and Krill (20pts)
 
     # s 484
-
-def run5():
-    pass
     # Push Coral Reef and fill Boat (35pts) 15pts coral + 20pts samples
 
-    # s 519
+def run5():
 
     # Push Boat to port latch (s 20pts)
+    robot.turn(-2)
+    robot.drive(300)
+    robot.turn(-3)
+    robot.drive(300)
+    robot.drive(780, 140)
+    robot.turn(40)
+    robot.drive(200)
+    robot.curve(200, 45)
+    robot.turn(115)
 
     # s 539
 
 def run6():
-    pass
 
     # Dump Krill (s 50pts)
+    robot.turn(80)
+    robot.drive(-635)
+    robot.turn(-125)
+    robot.drive(100)
+    robot.drive(60, 100)
 
     # s 589 pts
 
     # Reveal West Whale (s 10pts)
+    robot.curve(-340, -100)
+    robot.turn(-124)
 
     # s 599pts
 
     # Park on Unknown Creature in cold seep (10pts)
+    robot.turn(20)
+    robot.drive(-300)
 
     # s 609s
 
@@ -426,9 +464,11 @@ def runA():  # 21s 40pts
     robot.fork_lift(-270, 9000)
 
 # !! DO NOT REMOVE THE FOLLOWING LINES !!
-run1()
-robot.run()
-run2()
-robot.run()
-# robot.menu(run1, run2)
-# robot.menu(run1, run2, run3, run4, run5, run6, run7)
+singlerun = False
+if singlerun:
+    run1()
+    robot.run()
+    run2()
+    robot.run()
+else:
+    robot.menu(run1, run2, run3, run4, run5, run6)
