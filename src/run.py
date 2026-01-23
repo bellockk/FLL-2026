153f405df6001587001e6ec8330a5e9d9308cc7f
FLL <fll@omms.edu>
2026-01-23 15:15:08 -0500

    Updates to all runs on Jaws

--- src/run.py
+++ src/run.py
@@ -1,6 +1,11 @@
 from robot import Robot
 robot = Robot()
 
+# robot.curve(300, 85)  #  Forwards right
+# robot.curve(1000, -20)  #  Forwards left
+# robot.curve(-1000, -20)  #  Backwards right
+# robot.curve(-1000, 20)  #  Backwards left
+
 def run1():
     # Pre-Run Initialization (Get Motor Endpoints for up and down)
     robot.initialize()
@@ -11,7 +16,7 @@ def run1():
 
 def run2():
     # Drive from Left for the Final Run (30 points)
-     robot.raise_all(400)
+     robot.plow(48)
      robot.drive(400,800)
      robot.plow(90)
      robot.drive(250,800)
@@ -39,67 +44,58 @@ def run2():
      robot.fork_lift(48)
      robot.drive(100,800)
      robot.turn(82)
-     robot.plow(80)
-     robot.drive(-260)
-#    robot.raise_all(1000)
- #   robot.drive(610,700)
-  #  robot.turn(-21)
-   # robot.drive(145,700)
-    #robot.turn(-36)
-    #robot.raise_all(1000)
+     robot.plow(90)
+     robot.drive(-360)
+     robot.turn(-30)
+     robot.raise_all(500)
+     robot.drive(-400,1000)
 
 
 def run3():
-    # Drive from Left Home to Salvage Operation
-    robot.fork_lift(60)
-    robot.plow(95)
-    robot.drive(-230,700)
-
-    # Clear the Sand
-    robot.fork_lift(48)
-    robot.drive(130,700)
-
-    # Raise Ship
-    robot.fork_lift(38)
-    robot.drive(-100)
-
-    # Drive to Right Launch Area
-    robot.plow(60)
-    robot.turn(-30)
-    robot.drive(-500,1000)
-    robot.turn(38)
-    robot.raise_all(1000)
-    robot.drive(-1000,1000)
-
-    # Angler Artifacts
-    angler_artifacts()  # <-- Comment just this line to remove angler artifacts
-
-
-def angler_artifacts():
-    robot.fork_lift(35)
-    robot.plow(60)
-    robot.drive(-370)
-    robot.plow(85)
-    robot.turn(8)
-    robot.drive(43)
-    robot.drive(-43)
-    robot.drive(43)
-    robot.drive(-43)
-    robot.drive(43)
-    robot.turn(-8)
-    robot.fork_lift(50)
-    robot.turn(13)
-    robot.drive(200)
-    robot.fork_lift(48)
-    robot.drive(-155)
-    robot.fork_lift(60)
-    robot.turn(-27)
-    robot.drive(-550)
-    robot.turn(22)
-    robot.drive(-900)
+   # robot.curve(600, 30)  #  Forwards right
+    robot.drive(540, 1000)
+    robot.curve(300, -40)  #  Forwards left
+    robot.drive(-50)
+    robot.curve(-500, -90)  #  Backwards right
 
 
 def run4():
+    # Drive from Left for the Final Run (30 points)
+    robot.plow(44)
+    robot.drive(190,1000)
+    robot.turn(44)
+    robot.drive(235,500)
+    robot.drive(-40,100)
+    robot.plow(60)
+    robot.drive(-100,1000)
+    robot.turn(-45)
+    robot.drive(-200,1000)
+    robot.turn(-90)
+    robot.drive(-240,1000)
+    robot.fork_lift(46)
+    robot.plow(85)
+    robot.turn(20)
+    robot.drive(70,1000)
+    robot.drive(-70)
+    robot.drive(70,1000)
+    robot.drive(-70)
+    robot.drive(90,1000)
+    robot.drive(-20,1000)
+    robot.turn(-20)
+    robot.drive(95)
+    robot.turn(22)
+    robot.fork_lift(49)
+    robot.drive(140,1000)
+    robot.fork_lift(47)
+    robot.drive(-127,1000)
+    robot.raise_all(1000)
+    robot.turn(-35)
+    robot.drive(-400,1000)
+    robot.turn(37)
+    robot.drive(-1000,1000)
+
+
+def run5():
     # Drive to What's on Sale? (Wares)
     robot.raise_all(1000)
     robot.fork_lift(19)
@@ -156,7 +152,7 @@ def run4():
     robot.drive(650,1000)
 
 
-def run5():
+def run6():
     # # Drive from Right Launch Area to Tip the Scales
     robot.plow(70)
     robot.drive(-270,1000)
@@ -175,36 +171,13 @@ def run5():
     robot.drive(850,1000)
 
 
-    # robot.drive(-400,700)
-
-    # Remove the Scale Pan
-    # robot.fork_lift(8)
-    # robot.turn(-25)
-
-    # Drive to Left Launch Area
-    # robot.drive(-1350,1000)
-    # robot.raise_all(1000)
-
-
-def run6():
-    # Drive from Left for Artifact Drop
-    robot.raise_all(1000)
-    robot.drive(410,700)
-    robot.plow(75,700)
-
-    # Return to Left Launch Area
-    robot.drive(-410,1000)
-    robot.raise_all(1000)
-
-
 def run7():
-    # Drive from Left for the Final Run (30 points)
-    robot.raise_all(1000)
-    robot.drive(610,700)
-    robot.turn(-21)
-    robot.drive(145,700)
-    robot.turn(-36)
-    robot.raise_all(1000)
+    robot.raise_all(300)
+    robot.drive(-450,1000)
+    robot.turn(20)
+    robot.fork_lift(40,50)
+    robot.drive(300)
+
 
 # !! DO NOT REMOVE THE FOLLOWING LINES !!
 robot.menu(run1, run2, run3, run4, run5, run6, run7)
